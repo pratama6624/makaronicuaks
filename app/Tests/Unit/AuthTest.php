728c471d8f55e47a1a725e9ce08b11c2e1ccb17a
pratama6624 <scyoung6624@gmail.com>
2024-10-11 23:48:35 +0700

    Membuat unit test untuk proses authentikasi user

---
+++ app/Tests/Unit/AuthTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use CodeIgniter\Test\CIUnitTestCase;
+use App\Models\Auth;
+use CodeIgniter\Test\Mock\MockEmail;
+
+class AuthTest extends CIUnitTestCase
+{
+    
+}
